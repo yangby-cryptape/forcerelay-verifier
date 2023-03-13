006a04f3a2d6a9719bdb5e9ff8e9a19ad6090934
liyukun <liyukun@cryptape.com>
2023-03-13 20:53:44 +0800

    feat: change README.md

--- config/tests/config.rs
+++ config/tests/config.rs
@@ -3,7 +3,7 @@ use std::path::Path;
 
 #[test]
 fn test_load_full_config() {
-    let path = Path::new("./helios.toml");
+    let path = Path::new("./forceth.toml");
 
     let config = Config::from_file(&path.to_path_buf(), "mainnet", &Default::default());
     assert_eq!(config.ckb_rpc, "https://testnet.ckbapp.dev");
